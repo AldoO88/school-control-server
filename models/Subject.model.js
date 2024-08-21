a475ae46800fc02be0545b8b308ef5953c44388b
Aldo Gonzalez <aldogonjuarez@.com>
2024-08-21 12:25:45 -0600

    first commit

---
+++ models/Subject.model.js
@@ -0,0 +1,31 @@
+import { Schema, model } from 'mongoose'
+
+const SubjectSchema = new Schema({
+    name: {
+      type: String,
+      required: [true, 'Subject name is required'],
+      unique: true,
+      trim: true,
+    },
+    description:{
+      type: String,
+      trim: true
+    },
+    grade:{
+      type: String
+    },
+    hoursWeek:{
+        type: Number
+    },
+    maxHoursDay:{
+        type: Number
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+const Subject = model('Subject', SubjectSchema)
+
+module.exports = Subject;
