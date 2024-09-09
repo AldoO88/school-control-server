720061807e4f6be8909c3357187782f89dc9f526
Aldo Gonzalez <aldogonjuarez@.com>
2024-09-09 00:59:41 -0600

    fix answer model

--- controllers/answers.controller.js
+++ controllers/answers.controller.js
@@ -1,20 +1,24 @@
 const mongoose = require('mongoose');
 const Answer = require('../models/Answer.model');
 const User = require('../models/User.model');
+const Diagnostic = require('../models/Diagnostic.model');
 
 const createAnswer = async (req, res, next) => {
   const { answers, result } = req.body;
   const { category, userId } = req.params;
 
   try {
-    const responses = Object.keys(answers).map(questionNumber => ({
-      questionNumber: parseInt(questionNumber),  // Convertimos el número de pregunta a un número entero
-      selectedOption: answers[questionNumber].selectedOption,  // Extraemos la opción seleccionada
-      selectedIndex: answers[questionNumber].selectedIndex,  // Extraemos el índice seleccionado
-    }));
+    
+      const responses = Object.keys(answers).map(questionNumber => ({
+        questionNumber: parseInt(questionNumber),  // Convertimos el número de pregunta a un número entero
+        selectedOption: answers[questionNumber].selectedOption,  // Extraemos la opción seleccionada
+        selectedIndex: answers[questionNumber].selectedIndex,  // Extraemos el índice seleccionado
+      }));
+  
+      const newAnswer = await Answer.create({ test: category, responses, result, userId });
+      res.status(201).json(newAnswer);
 
-    const newAnswer = await Answer.create({ test: category, responses, result, userId });
-    res.status(201).json(newAnswer);
+    
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
